43a1da8b6e9d4ead1f7dc5b30ab2e470dc7807d1
crsvalle <cristian.valle@pursuit.org>
2024-05-27 10:28:31 -0400

    fixed name convention for import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
-import TeamsList from "./Pages/TeamsIndex";
+import TeamsIndex from "./Pages/TeamsIndex";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import NewTeams from "./Pages/NewTeams";
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/teams" element={<TeamsList />} />
+          <Route path="/teams" element={<TeamsIndex />} />
           <Route path="/teams/new" element={<NewTeams />} />
           <Route path="login" element={<Login />} />
           <Route path="/register" element={<Register />} />
